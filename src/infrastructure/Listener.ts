03612c0f9980a8728df1010eebf7dbe4da754106
jorisjaja <h.joris@gmail.com>
2018-09-12 12:11:09 +0200

    Fix handle error listener

--- src/infrastructure/Listener.ts
+++ src/infrastructure/Listener.ts
@@ -107,6 +107,7 @@ export class Listener {
                 this.webSocket.onerror = (err) => {
                     console.log('WebSocket Error ');
                     console.log(err);
+                    reject(err);
                 };
                 this.webSocket.onmessage = (msg) => {
                     const message = JSON.parse(msg.data as string);
