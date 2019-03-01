0a8bb8c54a463af33e5b935ea3b4f6cd7c81b246
Steven Liu <stevenliu@Stevens-MacBook-Pro.local>
2019-03-01 11:02:20 +0000

    added missed import for syncAnnounce

--- src/model/model.ts
+++ src/model/model.ts
@@ -79,6 +79,7 @@ export * from './transaction/RegisterNamespaceTransaction';
 export * from './transaction/SecretLockTransaction';
 export * from './transaction/SecretProofTransaction';
 export * from './transaction/SignedTransaction';
+export * from './transaction/SyncAnnounce';
 export * from './transaction/Transaction';
 export * from './transaction/TransactionAnnounceResponse';
 export * from './transaction/TransactionInfo';
