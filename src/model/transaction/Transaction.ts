9e511e1f166dde7030d1553db5e458118513ab38
Steven Liu <xian.f.liu@gmail.com>
2019-03-13 21:50:30 +0000

    Added serialize method for transaction

--- src/model/transaction/Transaction.ts
+++ src/model/transaction/Transaction.ts
@@ -26,7 +26,6 @@ import { InnerTransaction } from './InnerTransaction';
 import { SignedTransaction } from './SignedTransaction';
 import { TransactionInfo } from './TransactionInfo';
 import { TransactionType } from './TransactionType';
-import { SerializeTransactionToJSON } from '../../infrastructure/transaction/SerializeTransactionToJSON';
 
 /**
  * An abstract transaction class that serves as the base class of all NEM transactions.
