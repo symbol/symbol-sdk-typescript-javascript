ccf38293e3b6a3acfbbfee85ef79d3087b5b0d9c
Steven Liu <xian.f.liu@gmail.com>
2019-05-16 16:00:31 +0100

    Added #147 dragon features and DTOs

---
+++ src/infrastructure/DiagnosticHttps.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2019 NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {DiagnosticRoutesApi} from 'nem2-library';
+import {from as observableFrom, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {BlockchainStorageInfo} from '../model/blockchain/BlockchainStorageInfo';
+import { ServerInfo } from '../model/diagnostic/ServerInfo';
+import {DiagnosticRepository} from './DiagnosticRepository';
+import {Http} from './Http';
+
+/**
+ * Diagnostic http repository.
+ *
+ * @since 1.0
+ */
+export class DiagnosticHttp extends Http implements DiagnosticRepository {
+    /**
+     * @internal
+     * Nem2 Library diagnostic routes api
+     */
+    private diagnosticRoutesApi: DiagnosticRoutesApi;
+
+    /**
+     * Constructor
+     * @param url
+     */
+    constructor(url: string) {
+        super(url);
+        this.diagnosticRoutesApi = new DiagnosticRoutesApi(this.apiClient);
+    }
+
+    /**
+     * Gets blockchain storage info.
+     * @returns Observable<BlockchainStorageInfo>
+     */
+    public getDiagnosticStorage(): Observable<BlockchainStorageInfo> {
+        return observableFrom(
+            this.diagnosticRoutesApi.getDiagnosticStorage()).pipe(map((blockchainStorageInfoDTO) => {
+            return new BlockchainStorageInfo(
+                blockchainStorageInfoDTO.numBlocks,
+                blockchainStorageInfoDTO.numTransactions,
+                blockchainStorageInfoDTO.numAccounts,
+            );
+        }));
+    }
+
+    /**
+     * Gets blockchain server info.
+     * @returns Observable<Server>
+     */
+    public getServerInfo(): Observable<ServerInfo> {
+        return observableFrom(
+            this.diagnosticRoutesApi.getServerInfo()).pipe(map((serverDTO) => {
+            return new ServerInfo(serverDTO.serverInfo.restVersion,
+                serverDTO.serverInfo.sdkVersion);
+        }));
+    }
+}
