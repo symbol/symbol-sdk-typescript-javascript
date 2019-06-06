9a76ebcc2aec9e3be60a51e853b6c94cb6fb35d4
Steven Liu <xian.f.liu@gmail.com>
2019-06-06 17:23:51 +0100

    removed nem2-lib references

--- src/model/transaction/Transaction.ts
+++ src/model/transaction/Transaction.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { VerifiableTransaction } from 'nem2-library';
+import { VerifiableTransaction } from '../../core/transaction/VerifiableTransaction';
 import { SerializeTransactionToJSON } from '../../infrastructure/transaction/SerializeTransactionToJSON';
 import { Account } from '../account/Account';
 import { PublicAccount } from '../account/PublicAccount';
