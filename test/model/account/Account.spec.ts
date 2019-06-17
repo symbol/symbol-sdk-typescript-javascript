57a77bb0349c2ee8545d9864a6ff24d810068ed8
Steven Liu <xian.f.liu@gmail.com>
2019-06-17 14:38:09 +0100

    Fixed on keccak hashType on address creation

--- test/model/account/Account.spec.ts
+++ test/model/account/Account.spec.ts
@@ -15,7 +15,6 @@
  */
 
 import {expect} from 'chai';
-import {convert} from 'nem2-library';
 import {Account} from '../../../src/model/account/Account';
 import {NetworkType} from '../../../src/model/blockchain/NetworkType';
 import { HashType } from '../../../src/model/transaction/HashType';
@@ -41,7 +40,6 @@ describe('Account', () => {
         expect(account.address.plain()).not.to.be.equal(accountInformation.address);
         expect(account.address.plain()).to.be.equal('SB3AIJASMUZQK2X5LXISBMCCC6SC5PAURPSFZPCG');
     });
-
 
     it('should throw exception when the private key is not valid', () => {
         expect(() => {
