185797aa61efbc171790d2392970fd141e5308b1
Steven Liu <xian.f.liu@gmail.com>
2019-06-17 13:08:58 +0100

    Added #164 Added NodeHttp

---
+++ src/model/node/NodeInfo.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { NetworkType } from '../blockchain/NetworkType';
+import { RoleType } from './RoleType';
+/**
+ * The node info structure describes basic information of a node.
+ */
+export class NodeInfo {
+
+    /**
+     * @param publicKey
+     * @param port
+     * @param networkIdentifier
+     * @param version
+     * @param roles
+     * @param host
+     * @param friendlyName
+     */
+    constructor(/**
+                 * The public key used to identify the node.
+                 */
+                public readonly publicKey: string,
+                /**
+                 * The port used for the communication.
+                 */
+                public readonly port: number,
+                /**
+                 * The network identifier.
+                 */
+                public readonly networkIdentifier: NetworkType,
+                /**
+                 * The version of the application.
+                 */
+                public readonly version: number,
+                /**
+                 * The roles of the application.
+                 */
+                public readonly roles: RoleType,
+                /**
+                 * The IP address of the endpoint.
+                 */
+                public readonly host: string,
+                /**
+                 * The name of the node.
+                 */
+                public readonly friendlyName: string ) {}
+}
