c5e24abdef97903cb3ca63adb7d3083d92929c46
cl <1105120802@qq.com>
2019-07-05 17:13:17 +0800

    Initialize the vue project

--- Vue.config.js
+++ Vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
         ? './'
         : '/',
     // 输出文件目录
-    outputDir: '../www',
+    outputDir: './www',
     assetsDir: 'static',
     // eslint-loader 是否在保存的时候检查
     lintOnSave: true,
