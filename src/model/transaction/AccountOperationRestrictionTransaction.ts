21964b27a2388b3b045ee7c65cf883213134dab4
Steven Liu <xian.f.liu@gmail.com>
2019-08-15 12:12:33 +0100

    Refactored TransactionMapping - createTransactionFromPayload now use catbuffer

--- src/model/transaction/AccountOperationRestrictionTransaction.ts
+++ src/model/transaction/AccountOperationRestrictionTransaction.ts
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+import { SignSchema } from '../../core/crypto/SignSchema';
 import { Convert } from '../../core/format';
 import { AccountOperationRestrictionModificationBuilder } from '../../infrastructure/catbuffer/AccountOperationRestrictionModificationBuilder';
 import { AccountOperationRestrictionTransactionBuilder } from '../../infrastructure/catbuffer/AccountOperationRestrictionTransactionBuilder';
@@ -28,6 +29,7 @@ import { NetworkType } from '../blockchain/NetworkType';
 import { UInt64 } from '../UInt64';
 import { AccountRestrictionModification } from './AccountRestrictionModification';
 import { Deadline } from './Deadline';
+import { InnerTransaction } from './InnerTransaction';
 import { Transaction } from './Transaction';
 import { TransactionInfo } from './TransactionInfo';
 import { TransactionType } from './TransactionType';
@@ -86,6 +88,36 @@ export class AccountOperationRestrictionTransaction extends Transaction {
                 transactionInfo?: TransactionInfo) {
         super(TransactionType.ACCOUNT_RESTRICTION_OPERATION,
               networkType, version, deadline, maxFee, signature, signer, transactionInfo);
+    }
+
+    /**
+     * Create a transaction object from payload
+     * @param {string} payload Binary payload
+     * @param {Boolean} isEmbedded Is embedded transaction (Default: false)
+     * @param {SignSchema} signSchema The Sign Schema. (KECCAK_REVERSED_KEY / SHA3)
+     * @returns {Transaction | InnerTransaction}
+     */
+    public static createFromPayload(payload: string,
+                                    isEmbedded: boolean = false,
+                                    signSchema: SignSchema = SignSchema.SHA3): Transaction | InnerTransaction {
+        const builder = isEmbedded ? EmbeddedAccountOperationRestrictionTransactionBuilder.loadFromBinary(Convert.hexToUint8(payload)) :
+            AccountOperationRestrictionTransactionBuilder.loadFromBinary(Convert.hexToUint8(payload));
+        const signer = Convert.uint8ToHex(builder.getSigner().key);
+        const networkType = Convert.hexToUint8(builder.getVersion().toString(16))[0];
+        const transaction = AccountOperationRestrictionTransaction.create(
+            isEmbedded ? Deadline.create() : Deadline.createFromDTO(
+                (builder as AccountOperationRestrictionTransactionBuilder).getDeadline().timestamp),
+            builder.getRestrictionType().valueOf(),
+            builder.getModifications().map((modification) => {
+                return AccountRestrictionModification.createForOperation(
+                    modification.modificationAction.valueOf(),
+                    modification.value.valueOf(),
+                );
+            }),
+            networkType,
+            isEmbedded ? new UInt64([0, 0]) : new UInt64((builder as AccountOperationRestrictionTransactionBuilder).fee.amount),
+        );
+        return isEmbedded ? transaction.toAggregate(PublicAccount.createFromPublicKey(signer, networkType, signSchema)) : transaction;
     }
 
     /**
