cb000ffb1d9859d7a6c36a52f1c1b12fddf2dda8
Steven Liu <xian.f.liu@gmail.com>
2019-10-04 23:03:02 +0100

    JAV-52 [github #276] Fixed mosaicAmountView observable issue Fixed @types/long dev package in package.json

--- e2e/service/MosaicService.spec.ts
+++ e2e/service/MosaicService.spec.ts
@@ -1,4 +1,19 @@
-import { map, mergeMap, toArray } from 'rxjs/operators';
+/*
+ * Copyright 2019 NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { AccountHttp } from '../../src/infrastructure/AccountHttp';
 import { MosaicHttp } from '../../src/infrastructure/MosaicHttp';
 import { Address } from '../../src/model/account/Address';
@@ -22,15 +37,17 @@ describe('MosaicService', () => {
             done();
         });
     });
-    it('should return the mosaic list skipping the expired mosaics', () => {
+    it('should return the mosaic list skipping the expired mosaics', (done) => {
         const mosaicService = new MosaicService(accountHttp, mosaicHttp);
 
         const address = accountAddress;
 
-        return mosaicService.mosaicsAmountViewFromAddress(address).pipe(
-            mergeMap((_) => _),
-            map((mosaic) => console.log('You have', mosaic.relativeAmount(), mosaic.fullName())),
-            toArray(),
-        ).toPromise();
+        return mosaicService.mosaicsAmountViewFromAddress(address).subscribe((amountViews) => {
+            const views = amountViews.map((v) => {
+                return {mosaicId: v.fullName(), amount: v.relativeAmount()};
+            });
+            console.log(views);
+            done();
+        });
     });
 });
