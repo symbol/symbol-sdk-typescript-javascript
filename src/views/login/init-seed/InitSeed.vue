4069e1ba557c8992d325eb9cb523f51dfde454b8
NemTechCN <45383558+NemTechCN@users.noreply.github.com>
2019-10-11 10:38:36 +0800

    Task #331 create import button (#348)
    * Merge branch 'master' of https://github.com/nemfoundation/nem2-desktop-wallet into task_#141_mosaic_list_overflow
    
    # Conflicts:
    #	src/views/monitor/monitor-transfer/transactions/TransactionForm.less
    
    * update
    
    * Update README.md
    
    * hide create / import when one of them has been clicked

--- src/views/login/init-seed/InitSeed.vue
+++ src/views/login/init-seed/InitSeed.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="init_seed radius">
-    <div class="walletFnNav">
+    <div class="walletFnNav" v-if="pageIndex !== -1">
       <ul class="navList clear">
         <li :class="[item.active?'active':'','left']"
             v-for="(item,index) in navList"
             :key="index"
             @click="goToPage(item,index)"
-        >{{$t(item.name)}}
+        >
+          {{$t(item.name)}}
         </li>
       </ul>
     </div>
-    <div class="walletFnContent radius">
-      <SeedCreatedGuide
-              v-if="pageIndex === -1"
-              :createForm='createForm'
-              @updatePageIndex="updatePageIndex"
-      >
-      </SeedCreatedGuide>
-<!--      <AccountCreateMnemonic-->
-<!--              v-if="pageIndex === 0"-->
-<!--              @isCreated="isCreated"-->
-<!--              @updatePageIndex="updatePageIndex">-->
-<!--      </AccountCreateMnemonic>-->
+
+    <SeedCreatedGuide
+            v-if="pageIndex === -1"
+            :createForm='createForm'
+            @updatePageIndex="updatePageIndex"
+    >
+    </SeedCreatedGuide>
+
+    <div class="walletFnContent radius" v-else>
+
       <AccountImportMnemonic
               v-if="pageIndex === 1"
               @updatePageIndex="updatePageIndex"
