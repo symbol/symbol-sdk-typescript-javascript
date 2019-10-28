092d6a82f671cefac18494fe6335f20d0a73b597
Decentraliser <me@decentraliser.dev>
2019-10-28 09:26:04 +0700

    catch getMarketOpenPrice error, fixes #310 (#522)

--- src/core/services/marketData.ts
+++ src/core/services/marketData.ts
@@ -4,14 +4,18 @@ import {market} from "@/core/api"
 
 
 export const getMarketOpenPrice = async (that: any) => {
-  const rstStr = await market.kline({period: "1min", symbol: "xemusdt", size: "1"})
-  if (!rstStr.rst) return
-  const rstQuery: KlineQuery = JSON.parse(rstStr.rst)
-  const result = rstQuery.data ? rstQuery.data[0].close : 0
-  that.$store.commit('SET_XEM_USD_PRICE', result)
-  const openPriceOneMinute = {
-    timestamp: new Date().getTime(),
-    openPrice: result
+  try {
+      const rstStr = await market.kline({period: "1min", symbol: "xemusdt", size: "1"})
+      if (!rstStr.rst) return
+      const rstQuery: KlineQuery = JSON.parse(rstStr.rst)
+      const result = rstQuery.data ? rstQuery.data[0].close : 0
+      that.$store.commit('SET_XEM_USD_PRICE', result)
+      const openPriceOneMinute = {
+        timestamp: new Date().getTime(),
+        openPrice: result
+      }
+      localSave('openPriceOneMinute', JSON.stringify(openPriceOneMinute))
+  } catch (error) {
+      console.error("getMarketOpenPrice -> error", error)
   }
-  localSave('openPriceOneMinute', JSON.stringify(openPriceOneMinute))
 }
