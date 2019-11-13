f55489ff3f87d6de156013e9a50df1d45363d1c9
Fernando Boucquez <fboucquez@gmail.com>
2019-11-13 12:55:02 +0100

    Added merkleHashBuilder.ts for f2

---
+++ src/core/crypto/merkleHashBuilder.ts
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { SignSchema } from "./SignSchema";
+
+export class MerkleHashBuilder {
+
+    hashes: Array<Uint8Array> = new Array<Uint8Array>();
+    hasherFactory: any;
+    signSchema: SignSchema;
+    length: number;
+
+    constructor(hasherFactory: any, signSchema: SignSchema = SignSchema.SHA3, length: number = 32) {
+        this.hasherFactory = hasherFactory;
+        this.signSchema = signSchema;
+        this.length = length;
+    }
+
+    hash(hashes: Array<Uint8Array>): Uint8Array {
+        let hasher = this.hasherFactory(this.length, this.signSchema);
+        hasher.reset();
+
+        hashes.forEach(hash => {
+            hasher.update(hash);
+        });
+
+        const hash = new Uint8Array(this.length);
+        hasher.finalize(hash);
+        return hash;
+    }
+
+    calculateRootHash(hashes: Array<Uint8Array>): Uint8Array {
+
+        if (hashes.length == 0) {
+            return new Uint8Array(this.length);
+        }
+
+        var numRemainingHashes = hashes.length;
+        while (numRemainingHashes > 1) {
+            for (var i = 0; i < numRemainingHashes; i += 2) {
+                if (i + 1 < numRemainingHashes) {
+                    hashes.splice(i / 2, 0, this.hash([hashes[i], hashes[i + 1]]));
+                    continue;
+                }
+
+                // if there is an odd number of hashes, duplicate the last one
+                hashes.splice(i / 2, 0, this.hash([hashes[i], hashes[i]]));
+                ++numRemainingHashes;
+            }
+            numRemainingHashes /= 2;
+        }
+        return hashes[0];
+    }
+
+    getRootHash(): Uint8Array {
+        return this.calculateRootHash(this.hashes);
+    }
+
+    update(hash: Uint8Array): void {
+        this.hashes.push(hash);
+    }
+
+}
