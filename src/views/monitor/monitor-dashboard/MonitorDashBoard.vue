845151f47d6c1d0bea454e50fae4a99c88639b4a
NemTechCN <45383558+NemTechCN@users.noreply.github.com>
2019-11-28 20:39:28 +0800

    Update dashboard ui, fixes #258  (#658)
    Update dashboard ui, fixes #258

--- src/views/monitor/monitor-dashboard/MonitorDashBoard.vue
+++ src/views/monitor/monitor-dashboard/MonitorDashBoard.vue
@@ -1,39 +1,48 @@
 <template>
-  <div class="dash_board_container secondary_page_animate">
+  <div class="monitor-dashboard-wrapper radius secondary_page_animate">
     <div class="top_network_info">
-      <div class="left_echart radius">
-        <span class="trend">{{$t('XEM_market_trend_nearly_7_days')}}</span>
-        <span class="right">
-          <span>{{$t('The_total_market_capitalization')}} (USD)</span>
-          <span class="black">{{formatNumber(xemUsdPrice)}}</span>
-        </span>
-        <div>
-          <span class="right">
-              <span>{{$t('average_price')}}</span><span class="black">${{averagePrice}} </span>
-            <span>{{$t('yesterday')}}</span><span :class="riseRange < 0 ? 'red':'green'">{{riseRange}}%</span>
+      <div class="top_wallet_info">
+        <div class="netWork radius">
+          <div class="title">
+            <span class="title_txt">{{$t('nodes')}}</span>
+          </div>
+          <img src="@/common/img/monitor/network.png" alt/>
+          <span class="txt_info"><numberGrow :value="chainStatus.nodeNumber"></numberGrow></span>
+        </div>
+        <div class="block_height radius">
+          <div class="title">
+            <span class="title_txt">{{$t('blocks')}}</span>
+          </div>
+          <img src="@/common/img/monitor/block_height.png" alt/>
+          <span class="txt_info">
+            <numberGrow :value="currentHeight"></numberGrow>
           </span>
         </div>
-        <LineChart />
-      </div>
-      <div class="right_net_status radius">
-        <div class="panel_name">{{$t('network_status')}}</div>
-
-        <div
-          class="network_item radius"
-          v-for="(n, index) in networkStatusList"
-          :key="index"
-        >
-          <img :src="n.icon">
-          <span :class="['descript',index==1? 'long':'']">{{$t(n.descript)}}</span>
-          <span :class="['data','overflow_ellipsis', updateAnimation]">
-            <numberGrow v-if="index !== 4" :value="chainStatus[n.variable]"></numberGrow>
-            <span v-else>****{{chainStatus[n.variable].substr(-4,4)}}</span>
+        <div class="amount radius">
+          <div class="title">
+            <span class="title_txt">{{$t('transactions')}}</span>
+          </div>
+          <img src="@/common/img/monitor/amount.png" alt/>
+          <span class="txt_info">
+            <numberGrow :value="chainStatus.numTransactions"></numberGrow>
+          </span>
+        </div>
+        <div class="block_time radius">
+          <div class="title">
+            <span class="title_txt">{{$t('time')}}</span>
+            <span>
+              <numberGrow :value="chainStatus.targetBlockTime"></numberGrow>
+            </span>
+          </div>
+          <span class="txt_info">{{$t('recent_block')}}：</span>
+          <span class="txt_info2">
+            {{chainStatus.getTimeFromBlockNumber(chainStatus.currentHeight)}}
           </span>
         </div>
       </div>
     </div>
     <div class="transaction-list-wrapper">
-       <TransactionList />
+      <TransactionList/>
     </div>
 
   </div>
