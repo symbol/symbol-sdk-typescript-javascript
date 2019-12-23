70f3ab998519f0b48a13e1a697cad38856dd3ac9
Steven Liu <xian.f.liu@gmail.com>
2019-12-23 13:51:51 +0000

    Removed redundant switch case

--- src/infrastructure/catbuffer/EmbeddedTransactionHelper.ts
+++ src/infrastructure/catbuffer/EmbeddedTransactionHelper.ts
@@ -19,117 +19,38 @@
 *** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
 **/
 
-import { EntityTypeDto } from './EntityTypeDto';
-import { EmbeddedTransactionBuilder } from './EmbeddedTransactionBuilder';
-import { GeneratorUtils } from './GeneratorUtils';
-import { EmbeddedTransferTransactionBuilder } from './EmbeddedTransferTransactionBuilder';
-import { EmbeddedAccountLinkTransactionBuilder } from './EmbeddedAccountLinkTransactionBuilder';
-import { EmbeddedHashLockTransactionBuilder } from './EmbeddedHashLockTransactionBuilder';
-import { EmbeddedSecretLockTransactionBuilder } from './EmbeddedSecretLockTransactionBuilder';
-import { EmbeddedSecretProofTransactionBuilder } from './EmbeddedSecretProofTransactionBuilder';
-import { EmbeddedAccountMetadataTransactionBuilder } from './EmbeddedAccountMetadataTransactionBuilder';
-import { EmbeddedMosaicMetadataTransactionBuilder } from './EmbeddedMosaicMetadataTransactionBuilder';
-import { EmbeddedNamespaceMetadataTransactionBuilder } from './EmbeddedNamespaceMetadataTransactionBuilder';
-import { EmbeddedMosaicDefinitionTransactionBuilder } from './EmbeddedMosaicDefinitionTransactionBuilder';
-import { EmbeddedMosaicSupplyChangeTransactionBuilder } from './EmbeddedMosaicSupplyChangeTransactionBuilder';
-import { EmbeddedMultisigAccountModificationTransactionBuilder } from './EmbeddedMultisigAccountModificationTransactionBuilder';
-import { EmbeddedAddressAliasTransactionBuilder } from './EmbeddedAddressAliasTransactionBuilder';
-import { EmbeddedMosaicAliasTransactionBuilder } from './EmbeddedMosaicAliasTransactionBuilder';
-import { EmbeddedNamespaceRegistrationTransactionBuilder } from './EmbeddedNamespaceRegistrationTransactionBuilder';
 import { EmbeddedAccountAddressRestrictionTransactionBuilder } from './EmbeddedAccountAddressRestrictionTransactionBuilder';
+import { EmbeddedAccountLinkTransactionBuilder } from './EmbeddedAccountLinkTransactionBuilder';
+import { EmbeddedAccountMetadataTransactionBuilder } from './EmbeddedAccountMetadataTransactionBuilder';
 import { EmbeddedAccountMosaicRestrictionTransactionBuilder } from './EmbeddedAccountMosaicRestrictionTransactionBuilder';
 import { EmbeddedAccountOperationRestrictionTransactionBuilder } from './EmbeddedAccountOperationRestrictionTransactionBuilder';
+import { EmbeddedAddressAliasTransactionBuilder } from './EmbeddedAddressAliasTransactionBuilder';
+import { EmbeddedHashLockTransactionBuilder } from './EmbeddedHashLockTransactionBuilder';
 import { EmbeddedMosaicAddressRestrictionTransactionBuilder } from './EmbeddedMosaicAddressRestrictionTransactionBuilder';
+import { EmbeddedMosaicAliasTransactionBuilder } from './EmbeddedMosaicAliasTransactionBuilder';
+import { EmbeddedMosaicDefinitionTransactionBuilder } from './EmbeddedMosaicDefinitionTransactionBuilder';
 import { EmbeddedMosaicGlobalRestrictionTransactionBuilder } from './EmbeddedMosaicGlobalRestrictionTransactionBuilder';
+import { EmbeddedMosaicMetadataTransactionBuilder } from './EmbeddedMosaicMetadataTransactionBuilder';
+import { EmbeddedMosaicSupplyChangeTransactionBuilder } from './EmbeddedMosaicSupplyChangeTransactionBuilder';
+import { EmbeddedMultisigAccountModificationTransactionBuilder } from './EmbeddedMultisigAccountModificationTransactionBuilder';
+import { EmbeddedNamespaceMetadataTransactionBuilder } from './EmbeddedNamespaceMetadataTransactionBuilder';
+import { EmbeddedNamespaceRegistrationTransactionBuilder } from './EmbeddedNamespaceRegistrationTransactionBuilder';
+import { EmbeddedSecretLockTransactionBuilder } from './EmbeddedSecretLockTransactionBuilder';
+import { EmbeddedSecretProofTransactionBuilder } from './EmbeddedSecretProofTransactionBuilder';
+import { EmbeddedTransactionBuilder } from './EmbeddedTransactionBuilder';
+import { EmbeddedTransferTransactionBuilder } from './EmbeddedTransferTransactionBuilder';
+import { EntityTypeDto } from './EntityTypeDto';
+import { GeneratorUtils } from './GeneratorUtils';
 
 export class EmbeddedTransactionHelper {
 
     public static serialize(transaction: EmbeddedTransactionBuilder): Uint8Array {
-        let byte: Uint8Array;
-        let padding: Uint8Array;
-        switch (transaction.type) {
-            case EntityTypeDto.TRANSFER_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedTransferTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.ACCOUNT_LINK_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedAccountLinkTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.HASH_LOCK_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedHashLockTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.SECRET_LOCK_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedSecretLockTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.SECRET_PROOF_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedSecretProofTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.ACCOUNT_METADATA_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedAccountMetadataTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.MOSAIC_METADATA_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedMosaicMetadataTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.NAMESPACE_METADATA_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedNamespaceMetadataTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.MOSAIC_DEFINITION_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedMosaicDefinitionTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.MOSAIC_SUPPLY_CHANGE_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedMosaicSupplyChangeTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.MULTISIG_ACCOUNT_MODIFICATION_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedMultisigAccountModificationTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.ADDRESS_ALIAS_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedAddressAliasTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.MOSAIC_ALIAS_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedMosaicAliasTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.NAMESPACE_REGISTRATION_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedNamespaceRegistrationTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.ACCOUNT_ADDRESS_RESTRICTION_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedAccountAddressRestrictionTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.ACCOUNT_MOSAIC_RESTRICTION_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedAccountMosaicRestrictionTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.ACCOUNT_OPERATION_RESTRICTION_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedAccountOperationRestrictionTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.MOSAIC_ADDRESS_RESTRICTION_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedMosaicAddressRestrictionTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            case EntityTypeDto.MOSAIC_GLOBAL_RESTRICTION_TRANSACTION_BUILDER:
-                byte = (transaction as EmbeddedMosaicGlobalRestrictionTransactionBuilder).serialize();
-                padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
-                return GeneratorUtils.concatTypedArrays(byte, padding);
-            default:
-                throw new Error(`Transaction type: ${transaction.type} not recognized.`);
-        }
+        const byte = transaction.serialize();
+        const padding = new Uint8Array(GeneratorUtils.getTransactionPaddingSize(byte.length, 8));
+        return GeneratorUtils.concatTypedArrays(byte, padding);
     }
 
-    public static loadFromBinary(bytes: Uint8Array):EmbeddedTransactionBuilder {
+    public static loadFromBinary(bytes: Uint8Array): EmbeddedTransactionBuilder {
         const header = EmbeddedTransactionBuilder.loadFromBinary(bytes);
         switch (header.getType()) {
             case EntityTypeDto.TRANSFER_TRANSACTION_BUILDER:
