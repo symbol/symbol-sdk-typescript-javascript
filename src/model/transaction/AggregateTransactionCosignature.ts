dd3672dd25d41a2d1356524cbb627df29e87f14c
Steven Liu <xian.f.liu@gmail.com>
2020-01-30 18:27:19 +0000

    Fixed AggregateTransactionCosignature missing AggregateTransactionCosignature reference

--- src/model/transaction/AggregateTransactionCosignature.ts
+++ src/model/transaction/AggregateTransactionCosignature.ts
@@ -15,6 +15,7 @@
  */
 
 import { PublicAccount } from '../account/PublicAccount';
+import { NetworkType } from '../blockchain/NetworkType';
 
 /**
  * Model representing cosignature of an aggregate transaction.
