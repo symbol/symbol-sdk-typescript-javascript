07ca58bebfc1e4d5b5cbf9f29f47da9228e583f4
Fernando Boucquez <fboucquez@gmail.com>
2020-03-06 15:18:46 -0300

    fixed build

--- travis/uploadArchives.sh
+++ travis/uploadArchives.sh
@@ -5,6 +5,7 @@ CURRENT_VERSION=$(npm run version --silent)
 NEW_VERSION="$CURRENT_VERSION-alpha-$(date +%Y%m%d%H%M)"
 
 echo "Uploading npm package version $NEW_VERSION"
+cp .npmrc.template $HOME/.npmrc
 
 npm version "$NEW_VERSION" --commit-hooks false --git-tag-version false
 
