1432f841caac7da11f432f0bdadc34121fe4495e
Karma <289142486@qq.com>
2020-03-16 22:39:44 +0800

    fixed mnemonic qrcode icon (#97)

--- src/components/ProtectedMnemonicQRButton/ProtectedMnemonicQRButton.vue
+++ src/components/ProtectedMnemonicQRButton/ProtectedMnemonicQRButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="backup-item" @click="onClickDisplay">
-      <div class="img-box radius"><img src="@/views/resources/img/symbol/QR.png"></div>
+      <div class="img-box radius"><img src="@/views/resources/img/symbol/QRcode.png"></div>
       <p class="access-name">{{ $t('wallets_backup_tile_title') }}</p>
       <p class="access-info">{{ $t('wallets_backup_tile_description') }}</p>
     </div>
