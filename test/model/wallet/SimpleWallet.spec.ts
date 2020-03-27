17b870df1f44dcd5a5dbe755dcc0739b8662b6bc
decentraliser <decentraliser@gmail.com>
2020-03-27 15:51:16 +0800

    add SimpleWallet.toDTO, fixes #504

--- test/model/wallet/SimpleWallet.spec.ts
+++ test/model/wallet/SimpleWallet.spec.ts
@@ -60,4 +60,12 @@ describe('SimpleWallet', () => {
         const account2 = simpleWallet2.open(password);
         expect(account).to.deep.equal(account2);
     });
+
+    it('should create a simple wallet DTO', () => {
+        const privateKey = '5149a02ca2b2610138376717daaff8477f1639796aa108b7eee83e99e585b250';
+        const password = new Password('password');
+        const simpleWallet = SimpleWallet.createFromPrivateKey('wallet-name', password, privateKey, NetworkType.MIJIN_TEST);
+        const simpleWalletDTO = simpleWallet.toDTO()
+        expect(simpleWalletDTO).to.deep.equal(JSON.parse(JSON.stringify(simpleWallet)))
+    })
 });
