ac9d04ef05db1c26700d363781dfebff161dd8ad
Gregory Saive <greg@evias.be>
2020-05-26 15:19:08 +0200

    nemtech/symbol-sdk-typescript-javascrip#588: Added webpack bundle build and configuration for browser compatibility with target window.SymbolSDK

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var PATHS = {
+  entryPoint: path.resolve(__dirname, 'index.ts'),
+  bundles: path.resolve(__dirname, 'release'),
+}
+
+var config = {
+  mode: 'production',
+  // These are the entry point of our library. We tell webpack to use
+  // the name we assign later, when creating the bundle. We also use
+  // the name to filter the second entry point for applying code
+  // minification via UglifyJS
+  entry: {
+    'symbol-sdk': [PATHS.entryPoint]
+  },
+  // The output defines how and where we want the bundles. The special
+  // value `[name]` in `filename` tell Webpack to use the name we defined above.
+  // We target a UMD and name it MyLib. When including the bundle in the browser
+  // it will be accessible at `window.MyLib`
+  output: {
+    path: PATHS.bundles,
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'SymbolSDK',
+    umdNamedDefine: true
+  },
+  // Add resolve for `tsx` and `ts` files, otherwise Webpack would
+  // only look for common JavaScript file extension (.js)
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  // Activate source maps for the bundles in order to preserve the original
+  // source when the user debugs the application
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  node: {
+    fs: 'empty',
+    tls: 'empty',
+    net: 'empty'
+  },
+  performance: { 
+    hints: false
+  }
+}
+
+module.exports = config;
