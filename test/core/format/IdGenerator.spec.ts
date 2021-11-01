ea774a6896d3fc5623422f326e4828fa33feaec7
Steven Liu <xian.f.liu@gmail.com>
2021-11-01 17:54:20 +0000

    fix: renamed publicTest to testnet

--- test/core/format/IdGenerator.spec.ts
+++ test/core/format/IdGenerator.spec.ts
@@ -38,72 +38,72 @@ const basicMosaicInfo = {
 const mosaicTestVector = [
     {
         mosaicNonce: 2039925808,
-        address_Public: 'NATNE7Q5BITMUTRRN6IB4I7FLSDRDWZA34SQ33Y',
-        address_PublicTest: 'TATNE7Q5BITMUTRRN6IB4I7FLSDRDWZA37JGO5Q',
-        mosaicId_Public: '044C577DBDD6DC71',
-        mosaicId_PublicTest: '1796754FB181EF1E',
+        address_mainnet: 'NATNE7Q5BITMUTRRN6IB4I7FLSDRDWZA34SQ33Y',
+        address_Testnet: 'TATNE7Q5BITMUTRRN6IB4I7FLSDRDWZA37JGO5Q',
+        mosaicId_Mainnet: '044C577DBDD6DC71',
+        mosaicId_Testnet: '1796754FB181EF1E',
     },
     {
         mosaicNonce: 1477337076,
-        address_Public: 'NDR6EW2WBHJQDYMNGFX2UBZHMMZC5PGL2YCZOQQ',
-        address_PublicTest: 'TDR6EW2WBHJQDYMNGFX2UBZHMMZC5PGL2YBO3KA',
-        mosaicId_Public: '7E45A001465DEEA0',
-        mosaicId_PublicTest: '5E55573E3EBBB596',
+        address_mainnet: 'NDR6EW2WBHJQDYMNGFX2UBZHMMZC5PGL2YCZOQQ',
+        address_Testnet: 'TDR6EW2WBHJQDYMNGFX2UBZHMMZC5PGL2YBO3KA',
+        mosaicId_Mainnet: '7E45A001465DEEA0',
+        mosaicId_Testnet: '5E55573E3EBBB596',
     },
     {
         mosaicNonce: 1921674920,
-        address_Public: 'NCOXVZMAZJTT4I3F7EAZYGNGR77D6WPTRH6SYIQ',
-        address_PublicTest: 'TCOXVZMAZJTT4I3F7EAZYGNGR77D6WPTRE3VIBQ',
-        mosaicId_Public: '28E680397FDD9336',
-        mosaicId_PublicTest: '2F05C98474E9B263',
+        address_mainnet: 'NCOXVZMAZJTT4I3F7EAZYGNGR77D6WPTRH6SYIQ',
+        address_Testnet: 'TCOXVZMAZJTT4I3F7EAZYGNGR77D6WPTRE3VIBQ',
+        mosaicId_Mainnet: '28E680397FDD9336',
+        mosaicId_Testnet: '2F05C98474E9B263',
     },
     {
         mosaicNonce: 812613930,
         publicKey: '2E834140FD66CF87B254A693A2C7862C819217B676D3943267156625E816EC6F',
-        address_Public: 'NATNE7Q5BITMUTRRN6IB4I7FLSDRDWZA34SQ33Y',
-        address_PublicTest: 'TATNE7Q5BITMUTRRN6IB4I7FLSDRDWZA37JGO5Q',
-        mosaicId_Public: '296994F01121AFC9',
-        mosaicId_PublicTest: '570FB3ED9379624C',
+        address_mainnet: 'NATNE7Q5BITMUTRRN6IB4I7FLSDRDWZA34SQ33Y',
+        address_Testnet: 'TATNE7Q5BITMUTRRN6IB4I7FLSDRDWZA37JGO5Q',
+        mosaicId_Mainnet: '296994F01121AFC9',
+        mosaicId_Testnet: '570FB3ED9379624C',
     },
     {
         mosaicNonce: 1456792364,
         publicKey: '4875FD2E32875D1BC6567745F1509F0F890A1BF8EE59FA74452FA4183A270E03',
-        address_Public: 'NDR6EW2WBHJQDYMNGFX2UBZHMMZC5PGL2YCZOQQ',
-        address_PublicTest: 'TDR6EW2WBHJQDYMNGFX2UBZHMMZC5PGL2YBO3KA',
-        mosaicId_Public: '14AA6D651D9081B4',
-        mosaicId_PublicTest: '3A334999B5C56073',
+        address_mainnet: 'NDR6EW2WBHJQDYMNGFX2UBZHMMZC5PGL2YCZOQQ',
+        address_Testnet: 'TDR6EW2WBHJQDYMNGFX2UBZHMMZC5PGL2YBO3KA',
+        mosaicId_Mainnet: '14AA6D651D9081B4',
+        mosaicId_Testnet: '3A334999B5C56073',
     },
     {
         mosaicNonce: 2843362027,
         publicKey: '9F780097FB6A1F287ED2736A597B8EA7F08D20F1ECDB9935DE6694ECF1C58900',
-        address_Public: 'NCOXVZMAZJTT4I3F7EAZYGNGR77D6WPTRH6SYIQ',
-        address_PublicTest: 'TCOXVZMAZJTT4I3F7EAZYGNGR77D6WPTRE3VIBQ',
-        mosaicId_Public: '4A0C3A1CA50F2CFC',
-        mosaicId_PublicTest: '23CE1EAEBE30195A',
+        address_mainnet: 'NCOXVZMAZJTT4I3F7EAZYGNGR77D6WPTRH6SYIQ',
+        address_Testnet: 'TCOXVZMAZJTT4I3F7EAZYGNGR77D6WPTRE3VIBQ',
+        mosaicId_Mainnet: '4A0C3A1CA50F2CFC',
+        mosaicId_Testnet: '23CE1EAEBE30195A',
     },
     {
         mosaicNonce: 1686841592,
         publicKey: '0815926E003CDD5AF0113C0E067262307A42CD1E697F53B683F7E5F9F57D72C9',
-        address_Public: 'NDZ4373ASEGJ7S7GQTKF26TIIMC7HK5EWFDDCHA',
-        address_PublicTest: 'TDZ4373ASEGJ7S7GQTKF26TIIMC7HK5EWEPHRSI',
-        mosaicId_Public: '75A9C882F9A89606',
-        mosaicId_PublicTest: '748A1BD01A40DCF8',
+        address_mainnet: 'NDZ4373ASEGJ7S7GQTKF26TIIMC7HK5EWFDDCHA',
+        address_Testnet: 'TDZ4373ASEGJ7S7GQTKF26TIIMC7HK5EWEPHRSI',
+        mosaicId_Mainnet: '75A9C882F9A89606',
+        mosaicId_Testnet: '748A1BD01A40DCF8',
     },
     {
         mosaicNonce: 4206379750,
         publicKey: '3683B3E45E76870CFE076E47C2B34CE8E3EAEC26C8AA7C1ED752E3E840AF8A27',
-        address_Public: 'NDI5I7Z3BRBAAHTZHGONGOXX742CW4W5QAZ4BMQ',
-        address_PublicTest: 'TDI5I7Z3BRBAAHTZHGONGOXX742CW4W5QCY5ZUA',
-        mosaicId_Public: '3E949FBC6D09833D',
-        mosaicId_PublicTest: '40B85E3227EE56A0',
+        address_mainnet: 'NDI5I7Z3BRBAAHTZHGONGOXX742CW4W5QAZ4BMQ',
+        address_Testnet: 'TDI5I7Z3BRBAAHTZHGONGOXX742CW4W5QCY5ZUA',
+        mosaicId_Mainnet: '3E949FBC6D09833D',
+        mosaicId_Testnet: '40B85E3227EE56A0',
     },
     {
         mosaicNonce: 2539328334,
         publicKey: '4F593111964B37A9CAC59D2A70BC959AE9269589B75FBD640145EB0038960540',
-        address_Public: 'NAA6RO4ZAPEDGTCVADE3G4C7SWAE3DBQ4SCMOAI',
-        address_PublicTest: 'TAA6RO4ZAPEDGTCVADE3G4C7SWAE3DBQ4RTFBQY',
-        mosaicId_Public: '1B5FFAEBF3C9602A',
-        mosaicId_PublicTest: '503EBA0288209C64',
+        address_mainnet: 'NAA6RO4ZAPEDGTCVADE3G4C7SWAE3DBQ4SCMOAI',
+        address_Testnet: 'TAA6RO4ZAPEDGTCVADE3G4C7SWAE3DBQ4RTFBQY',
+        mosaicId_Mainnet: '1B5FFAEBF3C9602A',
+        mosaicId_Testnet: '503EBA0288209C64',
     },
 ];
 
@@ -161,18 +161,18 @@ describe('id generator', () => {
         // @dataProvider mosaicTestVector
         it('generates correct mosaicId given nonce and address', () => {
             mosaicTestVector.map((row) => {
-                const addressPublic = Address.createFromRawAddress(row.address_Public);
-                const addressTest = Address.createFromRawAddress(row.address_PublicTest);
+                const addressPublic = Address.createFromRawAddress(row.address_mainnet);
+                const addressTest = Address.createFromRawAddress(row.address_Testnet);
 
                 // Assert:
                 expect(
                     MosaicId.createFromNonce(MosaicNonce.createFromNumber(row.mosaicNonce), addressPublic).toHex(),
-                    'Public',
-                ).to.deep.equal(row.mosaicId_Public);
+                    'Mainnet',
+                ).to.deep.equal(row.mosaicId_Mainnet);
                 expect(
                     MosaicId.createFromNonce(MosaicNonce.createFromNumber(row.mosaicNonce), addressTest).toHex(),
-                    'PublicTest',
-                ).to.deep.equal(row.mosaicId_PublicTest);
+                    'Testnet',
+                ).to.deep.equal(row.mosaicId_Testnet);
             });
         });
     });
